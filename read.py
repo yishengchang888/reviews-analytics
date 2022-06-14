aabc5b18cdabdcd1e10283107defec6ef76dd655
Yisheng Liu <goever.tw@gmail.com>
2022-06-14 13:14:26 +0800

    calc file length

---
+++ read.py
@@ -0,0 +1,17 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
+
+
+sum_len = 0
+for _ in data:
+    sum_len += len(_)
+    print(sum_len)
+print('檔案讀取完了，總共有 ', len(data), ' 筆資料')
+print('留言的平均長度為： ', sum_len / len(data))
+
